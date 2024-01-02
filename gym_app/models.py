8b314633cbc41bd837155149ec74a9f80570bd42
Abdulmumin Akinde <lanreywaju97@gmail.com>
2024-01-02 22:41:20 +0100

    Add more fields to gym_app models

--- gym_app/models.py
+++ gym_app/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.utils import timezone
+
 from users.models import CustomUser
 
 # Create your models here.
 
 
 class Gymmer(models.Model):
+    # Choice of different subscription categories
     DAY = "DY"
     THREE_DAYS = "TD"
     EVERYDAY = "ED"
@@ -13,7 +16,9 @@ class Gymmer(models.Model):
         THREE_DAYS: "Three days in a week",
         DAY: "One day",
     }
-    gymmer = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     subscription_plan = models.CharField(
         max_length=2, choices=SUBSCRIPTION_PLANS, default=DAY
     )
+    gymmer = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    subscription_is_active = models.BooleanField(default=False, null=False)
+    started = models.DateTimeField(auto_now_add=True, default=timezone.now)
